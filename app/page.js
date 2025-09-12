58e4db8f93c3925e9b408fd8659b24f918a4fed2
atharva2254 <atharva2254@gmail.com>
2025-09-12 12:52:42 +0530

    testing git

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <>
-   drgdg
-   </>
+    <>
+      <h1>Home Page</h1>
+      <Link
+        href={"/dashboard"}
+        className="text-sm text-red-500 bg-green-400 p-2"
+      >
+        Go to Dashboard
+      </Link>
+    </>
   );
 }
