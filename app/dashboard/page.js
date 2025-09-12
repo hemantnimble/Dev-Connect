a347165280a7b3a696c1f195143988a557a90c61
atharva2254 <atharva2254@gmail.com>
2025-09-12 13:05:16 +0530

    testing github

---
+++ app/dashboard/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <Link href={"/"}>Go to Home</Link>
+    </div>
+  );
+}
