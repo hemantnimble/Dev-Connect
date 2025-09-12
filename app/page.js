38aff809e59d8fa44451da91e42d8f97aaf40a56
atharva2254 <atharva2254@gmail.com>
2025-09-12 13:06:38 +0530

    changes made

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <>
-   drgdg
-   </>
+    <>
+      <div>
+        <h1>Home Page</h1>
+        <Link href={"/dashboard"}>Go to dashboard</Link>
+      </div>
+    </>
   );
 }
